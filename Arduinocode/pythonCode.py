1c99562b7259a7de79b1bb54d3829a929f2daec5
bluhb <35539085+bluhb@users.noreply.github.com>
2019-05-16 20:59:46 +0200

    Add files via upload

--- Arduinocode/pythonCode.py
+++ Arduinocode/pythonCode.py
@@ -8,12 +8,13 @@ arduino = serial.Serial('COM3', 9600)
 time.sleep(2)
 dataUser = 0
 led = 0
+wait = 1
 
 
 
 
 def ArduinoWrite():
-    global url
+    global url, wait
     try:
         req = requests.get(url)
         json = req.json()
@@ -21,13 +22,14 @@ def ArduinoWrite():
         r = json['r']
         g = json['g']
         b = json['b']
+        wait = json['wait']
 
         data = struct.pack('>BBB', r,g,b)
     except:
         r=0
         g=0
         b=0
-        data = data = struct.pack('>BBB', 0,0,0)
+        data = struct.pack('>BBB', 0,0,0)
     arduino.write(data)
     #print(r)
     #print(g)
@@ -36,4 +38,4 @@ def ArduinoWrite():
 
 while True:
     ArduinoWrite()
-    time.sleep(0.05)
+    time.sleep(wait)
