a7c22aea01991af2ba84216e55048f9bc2b009f5
Bluhb <rogiervtol@gmail.com>
2019-05-16 20:05:08 +0100

    update arduino webhook

--- Website/web.py
+++ Website/web.py
@@ -81,7 +81,6 @@ def arduinodata():
 	global adata
 	adata = config.color_rgb_arduino
 	adata['wait'] = config.wait
-	#comment to create changes
 	return jsonify(adata), 200
 
 @app.route("/color", methods=["POST"])
