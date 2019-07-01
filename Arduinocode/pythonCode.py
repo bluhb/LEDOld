e13a55c27c4339d3e43c37b14f01a2b619382bbc
bluhb <rogiervtol@gmail.com>
2019-07-01 15:55:00 +0200

    Updated arduino code to also read dht11 sensor. You need to install the library for this if you want to use it

--- Arduinocode/pythonCode.py
+++ Arduinocode/pythonCode.py
@@ -2,6 +2,7 @@ import serial
 import requests
 import time
 import struct
+import sys
 
 url = 'http://192.168.1.80:5000/arduinodata'
 arduino = serial.Serial('COM3', 9600)
@@ -10,9 +11,6 @@ dataUser = 0
 led = 0
 wait = 1
 
-
-
-
 def ArduinoWrite():
     global url, wait
     try:
@@ -20,9 +18,9 @@ def ArduinoWrite():
         json = req.json()
         color = json['color']
 		
-        r = color['r']
-        g = color['g']
-        b = color['b']
+        r = int(color['r'])
+        g = int(color['g'])
+        b = int(color['b'])
         wait = json['wait']
 
         data = struct.pack('>BBB', r,g,b)
@@ -31,7 +29,24 @@ def ArduinoWrite():
         g=0
         b=0
         data = struct.pack('>BBB', 0,0,0)
+    arduino.write( struct.pack('?', True))
     arduino.write(data)
+    return None
+
+def ArduinoRead(): 
+    b = arduino.readline()
+    string_n = b.decode()
+    string = string_n.rstrip()
+    print(string)
+    return None
+
+def init():
+    print('init')
+    for i in range(0,4):
+        arduino.write(struct.pack('?', False))
+        time.sleep(0.1)
+	
+	
 ##    print(r)
 ##    print(g)
 ##    print(b)
@@ -39,6 +54,26 @@ def ArduinoWrite():
 ##    print(json)
 ##    print('---------------')
 
+interval = 5000
+prevMilli = time.time_ns() // 1000000 
+
+init()
 while True:
-    ArduinoWrite()
-    time.sleep(wait)
+    try:
+        now = time.time_ns() // 1000000
+        if now - prevMilli >= interval:
+            prevMilli = now
+            ArduinoRead()
+        
+        ArduinoWrite()
+        
+        time.sleep(wait)
+			
+    except KeyboardInterrupt:
+        data = struct.pack('>BBB', 0,0,0)
+        arduino.write(struct.pack('?',True))
+        arduino.write(data)
+        arduino.close()
+        print('Quitting the program')
+        sys.exit()
+
