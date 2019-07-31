e77b455f191d8e386bd5dcd5d50bf14171c06605
bluhb <rogiervtol@gmail.com>
2019-07-31 20:00:46 +0100

    cleaned up the code

--- Arduinocode/pythonCode.py
+++ Arduinocode/pythonCode.py
@@ -8,24 +8,24 @@ import config
 from tinydb import TinyDB, Query
 import datetime
 
-
-
 url = 'http://127.0.0.1:5000/arduinodata'
 arduino = serial.Serial('/dev/ttyUSB0', 115200)
-time.sleep(5) #wait 5 seconds to initialize the connection
+time.sleep(5)  # wait 5 seconds to initialize the connection
 wait = 1
-interval = 300 #5 min interval for reading the arduino.
-prevMilli = time.time() // 1000000 
+interval = 300  # 5 min interval for reading the arduino.
+prevMilli = time.time() // 1000000
 now = time.time() // 1000000
 
+
 def get_serial_port():
-    return "/dev/"+os.popen("dmesg | egrep ttyACM | cut -f3 -d: | tail -n1").read().strip()
+    return "/dev/" + os.popen("dmesg | egrep ttyACM | cut -f3 -d: | tail -n1").read().strip()
+
 
 def ArduinoWrite():
     global url, wait
     try:
-        #req = requests.get(url)
-        #json = req.json()
+        # req = requests.get(url)
+        # json = req.json()
         color = config.color_rgb_arduino
         brightness = config.brightness
         r = int(color['r'] * brightness)
@@ -33,15 +33,15 @@ def ArduinoWrite():
         b = int(color['b'] * brightness)
         wait = config.wait
 
-        data = struct.pack('>BBB', r,g,b)
+        data = struct.pack('>BBB', r, g, b)
     except KeyboardInterrupt:
         Exit()
     except:
-        r=0
-        g=0
-        b=0
-        data = struct.pack('>BBB', 0,0,0)
-    arduino.write( struct.pack('?', True))
+        r = 0
+        g = 0
+        b = 0
+        data = struct.pack('>BBB', 0, 0, 0)
+    arduino.write(struct.pack('?', True))
     arduino.write(data)
     print(data)
     return None
@@ -55,36 +55,39 @@ def ArduinoRead():
         humidTempD = humidTempB.decode()
         humidTemp = humidTempD.rstrip()
         val = humidTemp.split(",")
-        json = {'temp':val[0], 'humid':val[1], 'LDR':val[2]}
+        json = {'temp': val[0], 'humid': val[1], 'LDR': val[2]}
         config.roomTemp = val[0]
         config.roomHumid = val[1]
         config.roomLDR = val[2]
-        sensorData.insert({'time':str(datetime.datetime.now()),'data':json})
+        sensorData.insert({'time': str(datetime.datetime.now()), 'data': json})
         print(json)
     except KeyboardInterrupt:
         Exit()
     except:
         pass
-		
+
     return None
 
+
 def Exit():
-    data = struct.pack('>BBB', 0,0,0)
-    arduino.write(struct.pack('?',True))
+    data = struct.pack('>BBB', 0, 0, 0)
+    arduino.write(struct.pack('?', True))
     arduino.write(data)
     arduino.close()
     print('Quitting the program')
 
+
 def init():
     global sensorData, db
     print('init')
-    for i in range(0,4):
+    for i in range(0, 4):
         arduino.write(struct.pack('?', False))
         time.sleep(0.1)
     db = TinyDB('Database.json')
     sensorData = db.table('sensors')
     ArduinoRead()
     return None
+
 
 def main():
     global now, prevMilli
@@ -93,12 +96,13 @@ def main():
         if now - prevMilli >= interval:
             prevMilli = now
             ArduinoRead()
-        
+
         ArduinoWrite()
-        #time.sleep(wait)  
+        # time.sleep(wait)
     except KeyboardInterrupt:
         Exit()
     return None
+
 
 if __name__ == '__main__':
     print(get_serial_port())
